f6d9d7311c0e6027b3c5e649959163fbb51adf85
Sara Walker <sarawall1187@gmail.com>
2019-06-22 19:45:52 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,16 +1,14 @@
 class RepositoriesController < ApplicationController
 
   def index
-  
     github = GithubService.new({"access_token" => session[:token]})
-      binding.pry
     @username = github.get_username
     @repos_array = github.get_repos
 
   end
 
   def create
-    github = GithubService.new(session[:token])
+    github = GithubService.new({"access_token" => session[:token]})
     @new_repo = github.create_repo(params[:name])
     redirect_to '/'
   end
