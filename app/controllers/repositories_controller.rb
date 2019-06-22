169b7169424fe6b0cc78e807b09ba824aa87efd6
Sara Walker <sarawall1187@gmail.com>
2019-06-22 19:49:23 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,14 +1,15 @@
 class RepositoriesController < ApplicationController
 
   def index
-      github = GithubService.new
-      @username = github.get_username
-      @repos_array = github.get_repos
+    github = GithubService.new({"access_token" => session[:token]})
+    @username = github.get_username
+    @repos_array = github.get_repos
+
   end
 
   def create
-    github = GithubService.new
-    @new_repo = github.create_repo
+    github = GithubService.new({"access_token" => session[:token]})
+    @new_repo = github.create_repo(params[:name])
     redirect_to '/'
   end
 end
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -9,7 +9,6 @@ class GithubService
     response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: client_id, client_secret: client_secret, code: code}, {'Accept' => 'application/json'}
     access_hash = JSON.parse(response.body)
     @access_token = access_hash["access_token"]
-
   end
 
   def get_username
@@ -20,11 +19,13 @@ class GithubService
 
   def get_repos
     repos = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{@access_token}", 'Accept' => 'application/json'}
-    JSON.parse(repos.body)
+    JSON.parse(repos.body).map do |repo|
+       GithubRepo.new(repo)
+    end
   end
 
-  def create_repo
-      response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{@access_token}", 'Accept' => 'application/json'}
+  def create_repo(name)
+     Faraday.post "https://api.github.com/user/repos", {name: name}.to_json, {'Authorization' => "token #{@access_token}", 'Accept' => 'application/json'}
   end
 
 end
