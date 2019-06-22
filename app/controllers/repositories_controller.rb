dba44c230c69c3b1079324f22100a8a838c16780
Sara Walker <sarawall1187@gmail.com>
2019-06-22 18:47:47 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,13 +1,13 @@
 class RepositoriesController < ApplicationController
 
   def index
-      github = GithubService.new
+      github = GithubService.new(@access_hash)
       @username = github.get_username
       @repos_array = github.get_repos
   end
 
   def create
-    github = GithubService.new
+    github = GithubService.new(@access_hash)
     @new_repo = github.create_repo(params[:name])
     redirect_to '/'
   end
