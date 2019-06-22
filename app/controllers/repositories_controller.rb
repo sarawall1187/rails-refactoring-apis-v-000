3a53da6d3a0a462a372c5a6e82be4801f8ba278a
Sara Walker <sarawall1187@gmail.com>
2019-06-22 19:23:55 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,9 @@
 class RepositoriesController < ApplicationController
 
   def index
-  
+  binding.pry
     github = GithubService.new(@access_token)
-      binding.pry
+      
     @username = github.get_username
     @repos_array = github.get_repos
 
