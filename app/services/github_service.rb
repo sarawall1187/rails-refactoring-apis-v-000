7cc2c709eeb1ebc4dd78ff43935b3dc074b6f7b4
Sara Walker <sarawall1187@gmail.com>
2019-06-22 16:43:20 +0000

    Done.

---
+++ app/services/github_service.rb
@@ -0,0 +1,31 @@
+class GithubService
+  attr_accessor :access_token
+
+  def initialize(access_hash = {})
+    @access_token = access_hash['access_token']
+  end
+
+  def authenticate!(client_id, client_secret, code)
+    response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: client_id, client_secret: client_secret, code: code}, {'Accept' => 'application/json'}
+    access_hash = JSON.parse(response.body)
+    @access_token = access_hash["access_token"]
+
+  end
+
+  def get_username
+    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{@access_token}", 'Accept' => 'application/json'}
+    user_json = JSON.parse(user_response.body)
+    user_json["login"]
+  end
+
+  def get_repos
+    repos = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{@access_token}", 'Accept' => 'application/json'}
+    # binding.pry
+    JSON.parse(repos.body)
+  end
+
+  def create_repo
+      response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+  end
+
+end
