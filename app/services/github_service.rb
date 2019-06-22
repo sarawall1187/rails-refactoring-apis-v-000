22b4c3e47c7ae04f292bec546cdb1ed6902af247
Sara Walker <sarawall1187@gmail.com>
2019-06-22 18:41:43 +0000

    Automatically backed up by Learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -21,7 +21,10 @@ class GithubService
   def get_repos
     repos = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{@access_token}", 'Accept' => 'application/json'}
     binding.pry
-    JSON.parse(repos.body)
+    
+    JSON.parse(repos.body).map do |repo|
+       GithubRepo.create(repo)
+    end
   end
 
   def create_repo(name:)
